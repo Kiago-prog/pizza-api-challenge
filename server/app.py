3be26a83b4328363d0d7fad7351a2d3eda235ae5
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-06-15 15:17:02 +0300

    app.py

--- server/app.py
+++ server/app.py
@@ -1,12 +1,22 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
+from .models import db
+from .controllers.restaurant_controller import restaurant_bp
+from .controllers.pizza_controller import pizza_bp
+from .controllers.restaurant_pizza_controller import restaurant_pizza_bp
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def home():
-    return 'Hello, Flask!'
+    db.init_app(app)
+    Migrate(app, db)
+    CORS(app)
+
+    app.register_blueprint(restaurant_bp)
+    app.register_blueprint(pizza_bp)
+    app.register_blueprint(restaurant_pizza_bp)
+
+    return app
